a2ad5b5165389817362d828d74cea2f734a87707
luketevers <luketevers@gmail.com>
2019-09-30 18:38:24 -0700

    fixes(contactBar.js): updates schema for lifting anon chunk

--- studio/schemas/objects/contactBar.js
+++ studio/schemas/objects/contactBar.js
@@ -4,26 +4,19 @@ export default {
   title: 'Contact Bar',
   of: [
     {
-      title: 'Contact Item',
-      name: 'contactItem',
-      type: 'object',
-      fields: [
-        {
-          title: 'Label',
-          name: 'label',
-          type: 'string'
-        },
-        {
-          title: 'Href',
-          name: 'href',
-          type: 'string'
-        },
-        {
-          title: 'Icon',
-          name: 'icon',
-          type: 'string'
-        }
-      ]
+      title: 'Label',
+      name: 'label',
+      type: 'string'
+    },
+    {
+      title: 'Href',
+      name: 'href',
+      type: 'string'
+    },
+    {
+      title: 'Icon',
+      name: 'icon',
+      type: 'string'
     }
   ]
 }
