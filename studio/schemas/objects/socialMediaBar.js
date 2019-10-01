780ae3b16cd39ed379a7a47ea29b607ca8293927
luketevers <luketevers@gmail.com>
2019-09-30 18:40:40 -0700

    fixes(socialMediaBar.js): updates schema for lifting anon chunk

--- studio/schemas/objects/socialMediaBar.js
+++ studio/schemas/objects/socialMediaBar.js
@@ -4,26 +4,19 @@ export default {
   title: 'Social Media Bar',
   of: [
     {
-      title: 'Social Media Item',
-      name: 'socialMediaItem',
-      type: 'object',
-      fields: [
-        {
-          title: 'Label',
-          name: 'label',
-          type: 'string'
-        },
-        {
-          title: 'Href',
-          name: 'href',
-          type: 'string'
-        },
-        {
-          title: 'Icon',
-          name: 'icon',
-          type: 'string'
-        }
-      ]
+      title: 'Label',
+      name: 'label',
+      type: 'string'
+    },
+    {
+      title: 'Href',
+      name: 'href',
+      type: 'string'
+    },
+    {
+      title: 'Icon',
+      name: 'icon',
+      type: 'string'
     }
   ]
 }
